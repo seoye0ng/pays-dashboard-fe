8f1193a7b7292816316bb68b670b1f8b698bed5c
seoye0ng <bsy0313@gmail.com>
2025-11-28 05:21:41 +0900

    feat: 메인 레이아웃 구현 #7

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import { Outlet } from 'react-router-dom';
 
+import AppSidebar from '@/components/layout/Sidebar';
+import Header from '@/components/layout/Header';
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
+
 function App() {
   return (
-    <div>
-      <Outlet />
-    </div>
+    <SidebarProvider>
+      <AppSidebar variant='inset' />
+      <SidebarInset>
+        <Header />
+        <main className='p-6'>
+          <Outlet />
+        </main>
+      </SidebarInset>
+    </SidebarProvider>
   );
 }
 
