99df82ff888b613d639aaf7196d7f6d871d1c858
seoye0ng <bsy0313@gmail.com>
2025-11-28 12:23:24 +0900

    fix: api 요청 경로 수정

--- src/api/payments/requests.ts
+++ src/api/payments/requests.ts
@@ -2,7 +2,7 @@ import { getRequest } from '../httpRequests';
 import type { PaymentsResponse } from './type';
 
 export const getPayments = async () => {
-  const { data } = await getRequest<PaymentsResponse>('/api/v1/merchants/list');
+  const { data } = await getRequest<PaymentsResponse>('/api/v1/payments/list');
 
   return data;
 };
